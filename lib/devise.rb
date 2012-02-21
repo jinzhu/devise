0f2b418f7fd9679d3cc6bd3c444b24df1f1864dc
Jinzhu <wosmvp@gmail.com>
2012-02-21 21:13:17 +0800

    update devise for new rails

--- lib/devise.rb
+++ lib/devise.rb
@@ -393,7 +393,7 @@ module Devise
 
   # Generate a friendly string randomically to be used as token.
   def self.friendly_token
-    ActiveSupport::SecureRandom.base64(15).tr('+/=', 'xyz')
+    SecureRandom.base64(15).tr('+/=', 'xyz')
   end
 
   # constant-time comparison algorithm to prevent timing attacks
--- lib/devise/models/database_authenticatable.rb
+++ lib/devise/models/database_authenticatable.rb
@@ -33,6 +33,7 @@ module Devise
 
       # Verifies whether an password (ie from sign in) is the user password.
       def valid_password?(password)
+        return false if self.encrypted_password.blank?
         bcrypt   = ::BCrypt::Password.new(self.encrypted_password)
         password = ::BCrypt::Engine.hash_secret("#{password}#{self.class.pepper}", bcrypt.salt)
         Devise.secure_compare(password, self.encrypted_password)
